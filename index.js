228d574321097e1595498a17fd04b61251c9b81b
manasa <manasa.mudham@gmail.com>
2023-03-14 13:35:51 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  //   Write your code here.
+  <div className="main-container">
+    <div>
+      <h1 className="heading">Super Over League</h1>;
+    </div>
+
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
